d8f7d21e29351ac5c939e459e2e1464acd24b35e
kkdinar <kkdinar1983@gmail.com>
2020-04-05 17:33:25 +0500

    add sqlite3_linux

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 //app.listen()
 module.exports.main_port = 3000;
 
-//Подключение к БД sqlite3/main.db
+//Подключение к БД sqlite3/main.db Windows
+//module.exports.dbConfig = {
+//    dialect: 'sqlite',
+//    storage: './sqlite3/main.db'
+//};
+
 module.exports.dbConfig = {
     dialect: 'sqlite',
-    storage: './sqlite3/main.db'
+    storage: './sqlite3_linux/main.db'
 };
